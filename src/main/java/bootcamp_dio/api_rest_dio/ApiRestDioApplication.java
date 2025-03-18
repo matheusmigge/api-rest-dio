1460e8c2cc9bfe8f93f6e74924bd7a0c7114beab
Matheus Migge <matheusmigge@gmail.com>
2025-03-18 20:13:23 -0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/bootcamp_dio/api_rest_dio/ApiRestDioApplication.java
@@ -0,0 +1,13 @@
+package bootcamp_dio.api_rest_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp_dio/api_rest_dio/ApiRestDioApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp_dio.api_rest_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
