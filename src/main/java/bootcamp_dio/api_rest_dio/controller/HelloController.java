a4962ea35323a57bbb3cd0c6f65465383c42ff17
Matheus Migge <matheusmigge@gmail.com>
2025-03-18 21:39:05 -0300

    Add HelloController with a helloWorld endpoint

---
+++ src/main/java/bootcamp_dio/api_rest_dio/controller/HelloController.java
@@ -0,0 +1,15 @@
+package bootcamp_dio.api_rest_dio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
