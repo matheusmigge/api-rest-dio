a7627becc359f294736a62fb41e99f916d9e6059
Matheus Migge <matheusmigge@gmail.com>
2025-03-19 19:31:59 -0300

    Remove OpenAPI definition annotations from Application class

--- src/main/java/bootcamp_dio/api_rest_dio/Application.java
+++ src/main/java/bootcamp_dio/api_rest_dio/Application.java
@@ -2,10 +2,7 @@ package bootcamp_dio.api_rest_dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
